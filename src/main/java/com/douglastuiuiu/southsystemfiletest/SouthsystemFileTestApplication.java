b51f3c284f4e4f0a69fc3c07f24b74593aa87ee9
Douglas Gomes <douglasog87@gmail.com>
2020-09-29 13:40:46 -0300

    first commit

---
+++ src/main/java/com/douglastuiuiu/southsystemfiletest/SouthsystemFileTestApplication.java
@@ -0,0 +1,13 @@
+package com.douglastuiuiu.southsystemfiletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouthsystemFileTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouthsystemFileTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douglastuiuiu/southsystemfiletest/SouthsystemFileTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglastuiuiu.southsystemfiletest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouthsystemFileTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
